d25a9c7c7c60bf08de022dc570c6501e2a118bd8
Izurays <izuraysonyeocha@gmail.com>
2022-12-04 22:46:54 +0100

    d

--- app.js
+++ app.js
@@ -69,17 +69,7 @@ const store =  mongoDBstore.create({
     //touchAfter: 24 * 60 * 60
 //});
 
-app.use(
-    session({
-      secret: "thisismysecret",
-      resave: false,
-      saveUninitialized: false,
-      store: MongoDbStore.create({
-        mongoUrl: "mongodb://localhost:27017/pizza",
-      }),
-      cookie: { maxAge: 1000 * 60 * 60 * 24 }, //cookie valid for 24 hours
-    })
-  );
+
 
 store.on("error", function (e) {
     console.log("SESSION STORE ERROR", e)
